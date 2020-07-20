e966748ceb8d83514301a36da2cbf3f0701ee9d8
Vladimir Zhulin <justjestervz@gmail.com>
2020-07-20 07:19:57 +0300

    add timebar and soundbar

--- VideoCollection/View/ContentView.xaml.cs
+++ VideoCollection/View/ContentView.xaml.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using VideoCollection.Model;
 
 namespace VideoCollection.View
@@ -24,10 +27,25 @@ namespace VideoCollection.View
     {
         private bool IsPaused = true;
         private List<VideoDataTemplete> videoDataTempleteList = new List<VideoDataTemplete>();
+        bool? dialogOk;
+        OpenFileDialog fileDialog;
+        DispatcherTimer timer;
         public ContentView()
         {
             DataContext = this;
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += new EventHandler(timer_tick);
             InitializeComponent();
+            OpenFileSystemAsync();
+        }
+        void timer_tick(object sender, EventArgs e)
+        {
+            TimeBarSlider.Value = VideoOutput.Position.TotalSeconds;
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -45,28 +63,36 @@ namespace VideoCollection.View
                 VideoControlButton.Content = "Play";
             }
         }
-
-        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        private async void OpenFileSystemAsync()
         {
-            OpenFileDialog fileDialog = new OpenFileDialog();
-            fileDialog.Multiselect = true;
-            bool? dialogOk = fileDialog.ShowDialog();
-            if (dialogOk == true)
+            await Task.Run(() => OpenFileSystem());
+            if(dialogOk == true)
             {
-                foreach (string sFileName in fileDialog.FileNames)
-                {
-                    videoDataTempleteList.Add(new VideoDataTemplete()
-                    {
-                        Directory = sFileName,
-                        VideoName = RemoveSlash(sFileName)
-                    });
-                }
-                for(int i = 0; i < videoDataTempleteList.Count; i++)
-                {
-                    DataListView.Items.Add(videoDataTempleteList[i]);
-                }
+                FillingList();
             }
-
+        }
+        private void OpenFileSystem()
+        {
+            fileDialog = new OpenFileDialog
+            {
+                Multiselect = true
+            };
+            dialogOk = fileDialog.ShowDialog();
+        }
+        private void FillingList()
+        {
+            foreach (string sFileName in fileDialog.FileNames)
+            {
+                videoDataTempleteList.Add(new VideoDataTemplete()
+                {
+                    Directory = sFileName,
+                    VideoName = RemoveSlash(sFileName)
+                });
+            }
+            for (int i = 0; i < videoDataTempleteList.Count; i++)
+            {
+                DataListView.Items.Add(videoDataTempleteList[i]);
+            }
         }
         private string RemoveSlash(string value)
         {
@@ -99,9 +125,27 @@ namespace VideoCollection.View
             {
                 SourcePath = new Uri(videoDataTempleteList[this.DataListView.SelectedIndex].Directory)
             };
+            VideoOutput.Volume = (double)VolumeSlider.Value;
             VideoOutput.Stop();
             IsPaused = true;
             VideoControlButton.Content = "Play";
+        }
+
+        private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            VideoOutput.Volume = (double)VolumeSlider.Value;
+        }
+
+        private void TimeBarSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            VideoOutput.Position = TimeSpan.FromSeconds(TimeBarSlider.Value);
+        }
+
+        private void VideoOutput_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            TimeSpan time = VideoOutput.NaturalDuration.TimeSpan;
+            TimeBarSlider.Maximum = time.TotalSeconds;
+            timer.Start();
         }
     }
 }
